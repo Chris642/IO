d4aaedfecc9cec4dc254d6e29f704d9308d42231
happy2death <krzy.x.orcz@gmail.com>
2017-11-06 17:28:52 +0100

    LAB2
    LAB2 zad6-8

---
+++ iolab2/zad7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace iolab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("c://text/text.txt", System.IO.FileMode.Open);
+            byte[] buffer = new byte[1024];
+            var evt = new AutoResetEvent(false);
+            var magic = Tuple.Create(fs, buffer, evt);
+            IAsyncResult result = fs.BeginRead(buffer, 0, buffer.Length, null, magic);
+            fs.EndRead(result);
+            Tuple<FileStream, byte[], AutoResetEvent> wynik = (Tuple<FileStream, byte[], AutoResetEvent>)result.AsyncState;
+
+            byte[] bufor = wynik.Item2;
+
+
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(bufor));
+            FileStream fswynik = wynik.Item1;
+            fs.Close();
+            evt.Set();
+
+
+
+        }
+
+
+
+        static void myAsyncCallback(IAsyncResult nazwa)
+        {
+
+
+            Tuple<FileStream, byte[], AutoResetEvent> state = (Tuple<FileStream, byte[], AutoResetEvent>)nazwa.AsyncState;
+            byte[] bufor = state.Item2;
+
+
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(bufor));
+            FileStream fs = state.Item1;
+            var evt = state.Item3;
+            fs.Close();
+            evt.Set();
+
+
+
+        }
+
+
+
+    }
+}
---
+++ iolab2/zad8/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad8
+{
+    class Program
+    {
+
+        delegate Int64 DelegateType(Int64 arguments);
+        static DelegateType delegateName;
+        static Int64 Silniarek(Int64 arguments)
+        {
+            if (arguments < 1)
+                return 1;
+            else
+                return arguments * Silniarek(arguments - 1);
+
+            
+        }
+        static Int64 Silniaite(Int64 arguments)
+        {
+
+            Int64 result = 1;
+            for (Int64 i = 1; i <= arguments; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static Int64 Fiborek(Int64 arguments)
+        {
+
+            if ((arguments == 1) || (arguments == 2))
+                return 1;
+            else
+                return Fiborek(arguments - 1) + Fiborek(arguments - 2);
+        }
+
+        static Int64 Fiboite(Int64 arguments)
+        {
+
+            Int64 A = 0;
+            Int64 B = 1;
+
+            Int64 wynik = 0; 
+
+            if (arguments < 2) return arguments; 
+            
+            for (Int64 i = 2; i <= arguments; i++)
+            {
+                wynik = A + B; 
+                A = B; 
+                B = wynik;  
+            }
+
+            return wynik; 
+
+
+        }
+        static void Main(string[] args)
+        {
+            Int64 argu = 25;
+            Stopwatch ticktock = new Stopwatch();
+            ticktock.Start();
+
+            delegateName = new DelegateType(Silniarek);
+            IAsyncResult ar1 = delegateName.BeginInvoke(argu, null, null);
+            Int64 wsilniarek = delegateName.EndInvoke(ar1);
+
+            Console.WriteLine(ticktock.Elapsed);
+
+
+            delegateName = new DelegateType(Silniaite);
+            IAsyncResult ar2 = delegateName.BeginInvoke(argu, null, null);
+            Int64 wsilniaite = delegateName.EndInvoke(ar2);
+
+            Console.WriteLine(ticktock.Elapsed);
+
+            delegateName = new DelegateType(Fiborek);
+            IAsyncResult ar3 = delegateName.BeginInvoke(argu, null, null);
+            Int64 wfiborek = delegateName.EndInvoke(ar3);
+
+            Console.WriteLine(ticktock.Elapsed);
+
+
+            delegateName = new DelegateType(Fiboite);
+            IAsyncResult ar4 = delegateName.BeginInvoke(argu, null, null);
+            Int64 wfiboite = delegateName.EndInvoke(ar4);
+
+            Console.WriteLine(ticktock.Elapsed);
+
+
+            Console.WriteLine(wsilniarek);
+            Console.WriteLine(wsilniaite);
+            Console.WriteLine(wfiborek);
+            Console.WriteLine(wfiboite);
+
+
+        }
+    }
+}
